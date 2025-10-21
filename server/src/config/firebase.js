0d3fda9586b07979cf53a8365815b4717586775e
Ronse11 <seronronalddalida@gmail.com>
2025-10-21 09:52:33 +0800

    chore: update to load service account from render for deployment

--- server/src/config/firebase.js
+++ server/src/config/firebase.js
@@ -2,14 +2,12 @@
   import { readFileSync } from "fs";
 
   const serviceAccount = JSON.parse(
-    readFileSync("./secrets/hcm-time-tracking-48f97-firebase-adminsdk-fbsvc-92d68c6563.json", "utf8")
+    readFileSync("/etc/secrets/hcm-time-tracking-48f97-firebase-adminsdk-fbsvc-92d68c6563.json", "utf8")
   );
 
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
 
   export const db = admin.firestore();
   export const auth = admin.auth();

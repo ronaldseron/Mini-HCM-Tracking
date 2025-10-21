9c540191ba09e9d7ec1b8b3f4683b407c058e75a
Ronse11 <seronronalddalida@gmail.com>
2025-10-21 13:24:55 +0800

    chore: fixed the time in and out for time calculation

--- server/src/utils/attendanceUtils.js
+++ server/src/utils/attendanceUtils.js
@@ -5,7 +5,7 @@ export function calculateWorkMetrics(timeIn, timeOut, schedStart, schedEnd) {
   const sStart = timeToDecimal(schedStart);
   const sEnd = timeToDecimal(schedEnd);
 
-  console.log({ tIn, timeOut, sStart, sEnd });
+  console.log({ timeIn, timeOut, sStart, sEnd });
   console.log("T", { tIn, timeOut, sStart, sEnd });
 
   // Use current time if no timeOut
